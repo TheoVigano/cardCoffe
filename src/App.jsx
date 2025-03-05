85f598e3fadbaceb0773771883d6f93f4c31c403
Theo Vigano <135166649+TheoVigano@users.noreply.github.com>
2025-03-05 17:57:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Card} from "./components/card";
+import tradicional from "./assets/tradicional.png";
+
+
+export function App(){
+  return (
+    <div>
+      <Card coffeImg={tradicional} category="Tradicional" title="Expresso Tradicional" 
+      description="sdfnauilfbhklajfhiauhfkajfhilauhfçajklfhauçhf"/>
+    </div>
+  )
+}
